af64a0ab422062b095dc75eba5bbaa9d43973134
Coen Hallie <me@coenhallie.com>
2021-10-22 00:34:38 +0100

    add active state on buttons

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="flex flex-col justify-center gap-4">
-    <div class="flex flex-row justify-center gap-1">
-      <span style="display: none"> Selected range: {{selectedDatePickerRange}}</span>
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="setSelectedRange('DAILY')"> 1D </button>
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="setSelectedRange('WEEKLY')"> 1W </button>
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="setSelectedRange('MONTHLY')"> 1M </button>
+  <div class="flex flex-wrap justify-center gap-4" style="position: relative;">
+    <div class="flex flex-row justify-center gap-1" style="position: absolute; top: 40px; right: calc(50% - 500px); z-index: 1;">
+      <span style="display: none"> Selected range: {{selectedDatePickerRange}} {{selectedRange}}</span>
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" :class="{'bg-blue-700': selectedRange === 'DAILY'}" @click="setSelectedRange('DAILY')"> 1D </button>
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" :class="{'bg-blue-700': selectedRange === 'WEEKLY'}" @click="setSelectedRange('WEEKLY')"> 1W </button>
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" :class="{'bg-blue-700': selectedRange === 'MONTHLY'}" @click="setSelectedRange('MONTHLY')"> 1M </button>
       <input
         @change="setSelectedDatePickerRange"
         min='2021-04-08'
         max='2021-10-08'
         type="date"
+        class="date-picker"
       />
     </div>
-    <div class="shadow-md min-w-min">
+    <div class="chart-container flex flex-wrap content-center justify-center shadow-md min-w-min">
       <div id="chart"></div>
     </div>
   </div>
@@ -38,7 +39,7 @@ export default {
       selectedDatePickerRange.value = formatDate(event.target.value, selectedRange.value)
     }
 
-    function formatDate(date, type) {
+    const formatDate = (date, type) => {
       if (type === 'DAILY') {
         const [year, month, day] = date.split('-');
         return [{ year: parseInt(year), month: parseInt(month), date: parseInt(day) }, { year: parseInt(year), month: parseInt(month), date: parseInt(day) + 1 }]
@@ -53,7 +54,6 @@ export default {
       }
     }
 
-    // const xAxisScaleDomain = ref(deriveXAxisScaleDomain())
     const { lineChart } = useChartSpecification
 
     const chart = computed(() => {
@@ -67,7 +67,7 @@ export default {
       selectedRange,
       setSelectedRange,
       selectedDatePickerRange,
-      setSelectedDatePickerRange
+      setSelectedDatePickerRange,
     }
   }
 }
@@ -79,10 +79,13 @@ export default {
    border-radius: 10px;
  }
 
-.fade-enter-active, .fade-leave-active {
-  transition: opacity .5s;
+.date-picker {
+  border: 1px solid #3b82f6;
+  border-radius: 4px;
 }
-.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
-  opacity: 0;
+
+.chart-container {
+  height: 500px;
+  width: 1100px;
 }
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     extend: {},
   },
   variants: {
-    extend: {},
+    backgroundColor: ['active'],
   },
   plugins: [],
 }
